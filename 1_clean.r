70680c2a5133e0267bf39de945cb31c6290ec9c4
Emily Tumacder <69985014+emtinside@users.noreply.github.com>
2026-02-20 17:40:56 -0500

    fixed initial

--- 1_clean.r
+++ 1_clean.r
@@ -10,20 +10,21 @@ knitr::opts_chunk$set(echo = TRUE)
 
 ``` {r load libraries}
 library(dplyr)
+library(ggplot2)
 ```
 '''
 Research question: 
 '''
 ```{r load dataset}
 # Load demographics dataset
-demographics <- read.csv("stat512_casestudy1\\raw_data.csv")
+demographics <- read.csv("crime_data_with_poverty_tiers.csv")
 df_demographics <- data.frame(demographics)
 sprintf("Number of Observations (Demographics): %d", nrow(df_demographics))
 variables_demographics <- paste(names(df_demographics), collapse = ", ")
 sprintf("Categorical Variables: %s", variables_demographics)
 
 # Load crime dataset
-crimes <- read.csv("stat512_casestudy1\\crime_data_with_poverty_tiers.csv")
+crimes <- read.csv("crime_data_with_poverty_tiers.csv")
 df_crimes <- data.frame(crimes)
 sprintf("Number of Observations (Crimes): %d", nrow(crimes))
 variables_crime <- paste(names(df_demographics), collapse = ", ")
@@ -34,12 +35,12 @@ sprintf("Categorical Variables: %s", variables_crime)
 # Compute average percent of population under the poverty line (Crimes dataset)
 per_poverty_by_state <- df_crimes |>
   group_by(state) |>
-  summarise(state_percent_poverty = mean(per_poverty, na.rm = TRUE))
+  summarise(state_percent_poverty = mean(PctPopUnderPov, na.rm = TRUE))
 
 # Compute the average crime rate by state (Crimes dataset)
 crime_rate_by_state <- df_crimes |>
   group_by(state) |>
-  summarise(state_crime_rate = mean(crime_rate, na.rm = TRUE))
+  summarise(state_crime_rate = mean(totalCrimesPerPop, na.rm = TRUE))
 ```
 
 ```{r compute descriptive statistics + correlations}
